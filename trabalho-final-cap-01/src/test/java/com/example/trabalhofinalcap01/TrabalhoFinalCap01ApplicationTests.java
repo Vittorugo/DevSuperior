2e25831c9a1552864106ded707faec454a5820c8
vittorugo - pc 2 <hgloureirocosta@gmail.com>
2023-01-24 21:21:31 -0300

    Project created

---
+++ trabalho-final-cap-01/src/test/java/com/example/trabalhofinalcap01/TrabalhoFinalCap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabalhofinalcap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalCap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
